ff7328478559f0018776871b9f88226b21717f94
Aleksei Shashkov <AShashkov2@gmail.com>
2025-11-03 12:27:47 +0100

    First attempt of commiting files from Aleksei

--- .venv/Scripts/activate.fish
+++ .venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'c:\Repositories\immo-eliza-scraping\.venv'
+set -gx VIRTUAL_ENV 'c:\Repo\immo-eliza-scraping\.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
