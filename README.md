465a3fca26cf3c674bc5d90af3ba6f044a604f20
Faranges <farangesm2@gmail.com>
2025-11-10 09:03:10 +0100

    Faranges Branch

---
+++ README.md
@@ -0,0 +1,127 @@
+# 🏡 Immo Eliza Scraper - Data Collection Project
+
+[![Immo_Eliza_Scraping](https://images.pexels.com/photos/2157404/pexels-photo-2157404.jpeg)](https://www.pexels.com/nl-nl/foto/huizen-dichtbij-weg-2157404/) 
+
+*Image source: [PEXELS.COM] (https://www.pexels.com/nl-nl/foto/huizen-dichtbij-weg-2157404/)*
+
+
+## 📖 **Description**
+
+### Introduction
+
+Immo Eliza Scraper is a data collection project written in Python aimed at collecting high-quality real estate data across Belgium from the website Immovlan.be.
+
+In this project we built a dataset that gathers information about 15.000 property listings all over Belgium. This dataset will be used later to develop a machine learning model to predict property prices.
+
+### Dataset description
+Immo Elize Scraper collects data across 17 columns containing information about:
+
+Property ID,
+locality name,	
+postal code,
+property price,
+type of property (house/apartment),
+subtype,
+number of rooms,
+living area in m²,
+equipped kitchen,
+furnished,
+open fire,
+terrace (YES or NO),
+terrace	area in m²,
+garden	area in m²,
+number of facades,
+swimming pool and
+state of building	(e.g., new, to be renovated).
+
+
+### Quality Standards
+Data quality standards that are taken into account are:  
+* Minimum 10,000 unique properties
+* Properties distributed across all Belgium
+* No duplicates or empty rows
+* Missing values are encoded as None
+* Numerical values where possible
+* Consistent column naming
+
+## 📁 **Repo structure**
+
+```
+.
+├── Scraper
+├── Data
+│   ├── Raw
+│   ├── Cleaned
+├── main.py
+├── cleaned_data.csv
+├── README.md
+├── requirements.txt
+└── .gitignore
+```
+
+## 🚀 **Getting started**
+
+### Installation
+
+**1. Clone the project and navigate to the project folder**
+
+```
+cmd git clone https://github.com/butkutez/immo-eliza-scraping.git
+cd immo-eliza-scraping
+```
+
+**2. Install required packages**
+
+```
+pip install -r requirements.txt
+```
+
+The main Python libraries we used were Selenium for scraping dynamic sites and Pandas to structure the data and put it in a csv file. 
+
+**3. Run the main scraper**
+
+````
+python main.py
+````
+### Usage
+
+The script will:
+
+* Scrape property data from Immovlan.be.  
+* Clean and process the data.  
+* Save the final dataset to data/ folder.  
+* Generate a CSV file with the required columns.
+
+## 🌐 Sources
+https://immovlan.be/en - main property scraping source
+
+## 🎯 **Learning objectives**
+
+By the end of the project, we have:
+
+✅ Scraped websites for structured data using Selenium to work around dynamic content.    
+✅ Built datasets from scratch and managed the dataset.      
+✅ Collaborated in a team using Trello.    
+✅ Used Git effectively in a team setting.
+
+
+## 🗓 Timeline
+This project was finished in five days.  
+
+
+* Planning (assign roles, Trello setup, repo strategy): 1 day. 
+* Prototype: scrape 1-10 properties, test workflow:	1 days.
+* Scaling:	scrape 100 → 1,000 → 10,000 properties: 1 day.
+* Cleaning:	ensure consistency, remove duplicates	2 days.
+* Documentation:	README, comments, docstrings: 	1 day.
+* Final submission:	publish repo, dataset & presentation.
+  
+## 👥 Contributors
+Tim De Nijs (Project Manager)  
+Živilė Butkutė (Repo Manager)   
+Aleksei Shashkov (Presentation)  
+Faranges Mohamadi (README)  
+
+
+## 📌 Personal context note
+This project was done as part of the Data Science & AI course at BeCode Ghent, class of 2025-2026.
--- Tim_code/Tim_east_flanders.py
+++ Tim_code/Tim_east_flanders.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import csv
+import pandas as pd
 
 options = webdriver.EdgeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -13,27 +14,78 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Edge(options=options)
 wait = WebDriverWait(driver, 30)
 
+# ---------------------------
+# Provinces & property types
+# ---------------------------
 
-base_url = "https://immovlan.be/en/real-estate?transactiontypes=for-sale&propertytypes=house,apartment&provinces=namur&page={page}&noindex=1"
+def scrape_by_provencies():
+    all_data = []
+    property_links = []
 
-# Accept cookies once
-driver.get(base_url.format(page=1))
-try:
-    cookie_button = wait.until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div/div/div/div[2]/button[2]"))
-    )
-    cookie_button.click()
-except:
-    pass
+    # DELETE PROVINCES IN THE LIST THAT ARE NOT NEAR YOUR NAME BELOW
+    # zivile: antwerp, limburg
+    # faranges: east-flanders, flemish-brabant, brabant-wallon
+    # aleksei : west-flanders, Hainaut
+    # tim: namur, luxemburg, liege
 
-all_data = []
-property_links = []
 
+    provinces = ["vlaams-brabant"]
+
+    base_url = "https://immovlan.be/en/real-estate?transactiontypes=for-sale&propertytypes=house,apartment&provinces={province}&page={page}&noindex=1"
+    for province in provinces:
+        print(f"Scraping {province}")
+
+
+        for page in range(1, 51):
+            # Accept cookies once
+            driver.get(base_url.format(province=province, page=page))
+            try:
+                cookie_button = wait.until(
+                    EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div/div/div/div[2]/button[2]"))
+                )
+                cookie_button.click()
+            except:
+                pass
+
+            url = base_url.format(province=province, page=page)
+            driver.get(url)
+            time.sleep(1)
+
+            # call here the function of scrape_property
+            links = driver.find_elements(By.XPATH,"//a[contains(@href, '/detail/') or contains(@href, '/projectdetail/')]")
+            hrefs = [l.get_attribute("href") for l in links if l.get_attribute("href")]
+
+            detail_links = set([link for link in hrefs if "/detail/" in link])
+            project_links = set([link for link in hrefs if "/projectdetail/" in link])
+            # Add all detail links directly
+            for link in detail_links:
+                    if link not in property_links:
+                        property_links.append(link)
+            # Visit each unique project only once
+            for project in project_links:
+                driver.get(project)
+                time.sleep(3)
+                listings = driver.find_elements(By.XPATH, "//a[contains(@href, '/en/detail/')]")
+                hrefs = [p.get_attribute("href") for p in listings if p.get_attribute("href")]
+                for href in hrefs:
+                    if href not in property_links:
+                        property_links.append(href)
+            print(f" Collected {len(property_links)} unique property links in total.")
+
+        for link in property_links:
+            data = scrape_property(link)
+            all_data.append(data)
+            print(f"Scraped property from {province}: {link}")
+        # driver.close()
+
+
+    return all_data
 
 def scrape_property(property_url):
+    data = {}
+
     driver.get(property_url)
     time.sleep(2)  # Give the page a moment to load
-    data = {}
 
     try:
         data["property_ID"] = driver.find_element(By.XPATH, "//span[contains(@class, 'vlancode')]").text.strip()
@@ -166,75 +218,53 @@ def scrape_property(property_url):
 
     return data
 
-
-# -------------------------------
-# Get property links from first 10 pages
-# -------------------------------
-for page in range(1, 2):  # Pages 1
-    url = base_url.format(page=page)
-    driver.get(url)
-
-    try:
-        wait.until(lambda d: len(d.find_elements(By.XPATH, "//a[contains(@href, '/detail/')]")) > 0)
-    except:
-        continue  # skip if page fails to load
-
-    listings = driver.find_elements(By.XPATH, "//a[contains(@href, '/detail/')]")
-    for listing in listings:
-        link = listing.get_attribute("href")
-        if link not in property_links:
-            property_links.append(link)
-
-# -------------------------------
-# Scrape each property
-# -------------------------------
-for url in property_links:
-    property_data = scrape_property(url)
-    all_data.append(property_data)
-
-seen = set()
-unique_data = []
-for item in all_data:
-    locality = item["locality_name"]
-    postal = item["postal_code"]
-    price = item["price (€)"]
-    type = item["type"]
-    subtype = item["subtype"]
-    bedrooms = item["number_of_bedrooms"]
-    living = item["living_area (m²)"]
-    kitchen = item["equiped_kitchen (yes:1, no:0)"]
-    furnished = item["furnished (yes:1, no:0)"]
-    fire = item["open_fire (yes:1, no:0)"]
-    terrace = item["terrace (yes:1, no:0)"]
-    terrace_area = item["terrace_area (m²)"]
-    garden = item["garden (yes:1, no:0)"]
-    facades = item["number_facades"]
-    pool = item["swimming_pool (yes:1, no:0)"]
-    state = item["state_of_building"]
-    # only continue if locality_name is not empty or None
-    combo = (locality, postal, price, type, subtype, price, bedrooms, living, kitchen, furnished, fire, terrace, terrace_area, garden, facades, pool, state)
-    if combo not in seen:
-        seen.add(combo)
-        unique_data.append(item)
+def remove_duplicates(all_data):
+    seen = set()
+    unique_data = []
+    for item in all_data:
+        locality = item["locality_name"]
+        postal = item["postal_code"]
+        price = item["price (€)"]
+        type = item["type"]
+        subtype = item["subtype"]
+        bedrooms = item["number_of_bedrooms"]
+        living = item["living_area (m²)"]
+        kitchen = item["equiped_kitchen (yes:1, no:0)"]
+        furnished = item["furnished (yes:1, no:0)"]
+        fire = item["open_fire (yes:1, no:0)"]
+        terrace = item["terrace (yes:1, no:0)"]
+        terrace_area = item["terrace_area (m²)"]
+        garden = item["garden (yes:1, no:0)"]
+        facades = item["number_facades"]
+        pool = item["swimming_pool (yes:1, no:0)"]
+        state = item["state_of_building"]
+        # only continue if locality_name is not empty or None
+        combo = (locality, postal, price, type, subtype, price, bedrooms, living, kitchen, furnished, fire, terrace, terrace_area, garden, facades, pool, state)
+        if combo not in seen:
+            seen.add(combo)
+            unique_data.append(item)
+    return unique_data
 
 # -------------------------------
 # Save to CSV
 # -------------------------------
-keys = all_data[0].keys() if all_data else []
-with open("immo_eliza_west_flanders.csv", "w", newline="", encoding="utf-8") as f:
-    writer = csv.DictWriter(f, fieldnames=keys)
-    writer.writeheader()
-    writer.writerows(all_data)
 
-print(f"Scraped {len(all_data)} properties and saved to immo_eliza_west_flanders.csv")
+def save_to_csv():
+    all_data= scrape_by_provencies()
+    unique_data = remove_duplicates(all_data)
 
-keys = unique_data[0].keys() if unique_data else []
-with open("immo_eliza_unique.csv", "w", newline="", encoding="utf-8") as f:
-    writer = csv.DictWriter(f, fieldnames=keys)
-    writer.writeheader()
-    writer.writerows(unique_data)
+    df = pd.DataFrame(unique_data)
 
-print(f"Scraped {len(unique_data)} properties and saved to immo_eliza_unique.csv")
+    #EVERYONE!!! EVERY TIME YOU RUN A NEW SCRAPING, CHANGE CSV FILE NAME using this syntax 'province_yourname_1_number_of_scrapped_data.csv'
+
+    #ALEKSEI name your output file '_aleksei_1_number_of_scrapped_data.csv'
+    #FARANGES name your output file 'east_flanders_faranges_1_number_of_scrapped_data.csv'
+    #TIM name your output file 'namur_tim_1_number_of_scrapped_data.csv'
+    #ZIVILE name your output file 'antwerp_zivile_1_number_of_scrapped_data.csv'
+
+    df.to_csv('vlaams_brabant_faranges_1.csv', index=False)
+    print(f"Saved {len(unique_data)} unique records to vlaams_brabant_faranges_1.csv")
 
 
-driver.quit()
+if __name__ == "__main__": # Added now
+    save_to_csv()
